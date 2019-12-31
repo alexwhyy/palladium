0385c230ee394656c5afeaba3a8e86b0a64aac8d
WynnChairman <ericfang2002@gmail.com>
2019-12-31 14:52:10 -0500

    Create Access.java

---
+++ Access.java
@@ -0,0 +1,8 @@
+public abstract class Access{
+   String inputUsername;
+   String inputPassword;
+   
+   public abstract boolean checkUsername(String input);
+   public abstract boolean checkPassword(String input);
+   
+}
