19074f16c88023ba17e38366a5b54170d4a42544
WynnChairman <ericfang2002@gmail.com>
2020-01-07 14:09:34 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,21 +1,23 @@
 public class Login extends Access{
-   
-   public boolean checkUsername(String input){
-      if(userList.findUsername(input) >= 0){
-         return true;
-      }
-      else{
-         return false;
-      }
-   }
-   
-   public boolean checkPassword(String input){
-      if(userList.findUsername(input).password == inputPassword){
-         return true;
-      }
-      else{
-         return false;
-      }
-   }
-    
+
+	UserList ul = new UserList();
+	
+	public boolean checkUsername(){
+		if(ul.findUsername(inputUsername) >= 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean checkPassword(){
+		if(ul.findPassword(ul.findUsername(inputUsername)).compareTo(inputPassword) == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
 }
