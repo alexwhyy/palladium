55bd7b7994bdf7ba20000d04381c1e493a107498
Asphyxia <45414505+Asphyxia@users.noreply.github.com>
2020-01-09 14:10:59 -0500

    Created login and loginAccount

--- Login.java
+++ Login.java
@@ -1,23 +1,76 @@
-public class Login extends Access{
+import java.io.*;
 
-	UserList ul = new UserList();
-	
-	public boolean checkUsername(){
-		if(ul.findUsername(inputUsername) >= 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	public boolean checkPassword(){
-		if(ul.findPassword(ul.findUsername(inputUsername)).compareTo(inputPassword) == 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
+public class Login {
+    loginAccount [] list;
+    int numAccount;
+    final String WEAKPASSWORD = "Weak password \n Please make sure your password has: \n -More than 8 characters \n -Upper and lower letter \n -Numbers \n -Special characters";
+
+    public login() {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("src/main/accountList.txt"));
+            String input;
+            numAccount = Integer.parseInt(in.readLine());
+            list = new loginAccount[numAccount];
+            for (int i = 0; i < numAccount; i++) {
+                list[i] = new loginAccount(in.readLine(),in.readLine(),in.readLine());
+            }
+
+            in.close();
+        } catch (IOException iox) {
+            System.out.println("error");
+        }
+    }
+
+    public String compareLogin(String username, String password){
+        for(int i = 0 ; i < numAccount; i++){
+            if(username.compareTo(list[i].getUsername()) + password.compareTo(list[i].getPassword()) == 0){
+                return username;
+            }
+        }
+        return "not found";
+    }
+    public String forgetPassword (String username , String email){
+        for(int i = 0 ; i < numAccount ; i++){
+            if(username.compareTo(list[i].getUsername()) + email.compareTo(list[i].getEmail()) == 0 )
+                return list[i].getPassword();
+        }
+        return "Username does not exist or wrong email";
+    }
+
+    public String register(String username, String password){
+        try{
+            BufferedWriter out = new BufferedWriter(new FileWriter("src/main/accountList.txt",true));
+            for (int i = 0 ; i < numAccount ; i ++ ){
+                if(username.compareTo(list[i].getUsername()) == 0){
+                    if (checkPasswordStrength(password) ){
+                        out.newLine();
+                        out.write(username);
+                        out.newLine();
+                        out.write(password);
+                        return "registration successful";
+                    }else return WEAKPASSWORD;
+                }
+            }
+            return "Username taken";
+        }catch(IOException iox){
+            return "error";
+        }
+    }
+    private boolean checkPasswordStrength(String password){
+        boolean uppercase = false;
+        boolean lowercase = false;
+        boolean number = false;
+        boolean symbol = false;
+        for(int i =0 ; i < password.length(); i ++){
+            if(password.charAt(i) <= 'z' && password.charAt(i) >= 'a' ) lowercase = true;
+            else if(password.charAt(i) <= 'Z' && password.charAt(i) >= 'A' ) uppercase = true;
+            else if (password.charAt(i) <= '9' && password.charAt(i) >= '0' ) number = true;
+            else symbol = true;
+
+            }
+         return password.length() >= 8 &&uppercase  && lowercase  && number && symbol ;
+        }
+
+
+
 }
