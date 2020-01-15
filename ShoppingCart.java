4645a55c1be5b8f3acbbde5549f7ea9599173b2f
WynnChairman <ericfang2002@gmail.com>
2020-01-15 15:14:00 -0500

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,7 +1,20 @@
+/*
+Class Name: ShoppingCart
+Author: Eric Fang
+Date: Dec 18, 2019
+School: A.Y.Jackson Secondary School
+Purpose: Holds item that user has added and itends to purchase
+*/
 import java.util.ArrayList;
 public class ShoppingCart extends SavedProducts{
    public ArrayList<Product> shoppingCart = new ArrayList<Product>();
    
+   /*
+   Name: addToWishList
+   Purpose: moves a product from ShoppingCart to WishList
+   Description: passes in ArrayList(from WishList) and the position of product
+   in ShoppingCart
+   */
    public void addToWishList(int position, ArrayList<Product> wishList){
       wishList.add(shoppingCart.get(position));
       shoppingCart.remove(position);
