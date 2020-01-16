c987666f459e72b73b92469e639dbc8bc5e9abd7
WynnChairman <ericfang2002@gmail.com>
2020-01-16 13:24:01 -0500

    Update Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -1,5 +1,21 @@
+/*
+Class Name: Catalog
+Author: Eric Fang
+Date: Dec 18, 2019
+School: A.Y.Jackson Secondary School
+Purpose: Contains all Products the user can browse
+*/
 import java.util.ArrayList;
 
 public class Catalog extends SavedProducts {
-   public ArrayList<Product> arrayList = new ArrayList<Product>();
+   public ArrayList<Product> catalog = new ArrayList<Product>();
+   
+   /*
+   Name: Catalog
+   Purpose: Catalog constructor
+   Description: passes in ArrayList to initialize
+   */
+	public Catalog(ArrayList<Product> catalog) {
+		this.catalog = catalog;
+	}
 }
