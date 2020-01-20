bf5863afabd69ee05d662e75f78bf9c43562f91e
WynnChairman <ericfang2002@gmail.com>
2020-01-20 11:57:42 -0500

    Update Search.java

--- Search.java
+++ Search.java
@@ -31,32 +31,22 @@ public class Search{
             if (k == queryLength){
                returnList.add(catalog.get(i));
             }
-
          }
       }
       return returnList;
    }
    public static ArrayList<Product> searchProduct(String query, ArrayList<Product> catalog){
       ArrayList<Product> returnList = new ArrayList<Product>();   //list that will be returned
-      char queryArray[] = new char[query.length()];               //char array made using query
       query = query.toLowerCase();                                //query list initalization
-      for(int i = 0; i < query.length(); i++){
-         queryArray[i] = query.charAt(i);
-      }
       
-      for(int i = 0; i < catalog.size(); i++){                    //loops through all catalog entries
-      
-         char catalogArray[] = new char[catalog.get(i).name.length()];
+      for(int i = 0; i < catalog.size(); i++){                    //loops through all catalog entries      
+         
          String currentEntry = catalog.get(i).name.toLowerCase();
-         for(int j = 0; j < currentEntry.length(); j++){
-            catalogArray[j] = currentEntry.charAt(j);
-         }
-         //char array made with current catalog entry + intialization^^^
          for(int j = 0; j < catalog.get(i).name.length(); j++){  //loops through characters within current entry
             int holder = 0;
             try{                                                     
             //compares characters within querey char array with catalog product char array
-               if(catalogArray[j] == queryArray[0 + holder]){
+               if(currentEntry.charAt(j) == query.charAt(holder)){
                   holder++;
                }
                else{
