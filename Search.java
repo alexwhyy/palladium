28324d34947a19c4659842ed72dc12d71a52ace5
WynnChairman <ericfang2002@gmail.com>
2020-01-20 13:28:43 -0500

    Update Search.java

--- Search.java
+++ Search.java
@@ -7,59 +7,43 @@ Purpose: Searches catalog for matches and adds them to WishList/ShoppingCart
 */
 import java.util.ArrayList;
 public class Search{
-
+   
    /*
    Name: searchProduct
    Purpose: searches an ArrayList for all matches and returns an ArrayList containing them 
    Description: passes in String to search with and ArrayList to search through
    */
-   public static ArrayList<Product> searchProductK(String query,  ArrayList<Product> catalog){
-      ArrayList<Product> returnList = new ArrayList<Product>();
-      int queryLength = query.length();
-      int productLength;
-      query = query.toLowerCase();
-      String productName;
-      for(int i = 0 ; i < catalog.size(); i++ ){
-         productName = catalog.get(i).name.toLowerCase();
-         productLength = productName.length();
-         for(int j = 0 ; j <= productLength-queryLength; j ++){
-            int k;
-            for(k = 0 ; k < queryLength ; k ++){
-               if (productName.charAt(j + k) != query.charAt(k))
-                  break;
+   public static ArrayList<Product> searchProduct(String query, ArrayList<Product> catalog){
+      ArrayList<Product> returnList = new ArrayList<Product>();   //list that will be returned
+      query = query.toLowerCase();                                //query list initalization
+      for(int i = 0; i < catalog.size(); i++){                    //loops through all catalog entries      
+         
+         String currentEntry = catalog.get(i).name.toLowerCase();         
+         int holder = 0;
+         for(int j = 0; j < catalog.get(i).name.length(); j++){  //loops through characters within current entry
+            //compares characters within querey char array with catalog product char array
+            if(currentEntry.charAt(j) == query.charAt(holder)){
+               holder++;
             }
-            if (k == queryLength){
+            else{
+               holder = 0;
+            }
+            if(holder == query.length()){
                returnList.add(catalog.get(i));
+               holder = 0;
             }
+            
+            //if query array index out of bounds, all characters within the array have been compared and matched
+            //meaning it is a match and is added to the return list
+            
          }
       }
       return returnList;
    }
-   public static ArrayList<Product> searchProduct(String query, ArrayList<Product> catalog){
-      ArrayList<Product> returnList = new ArrayList<Product>();   //list that will be returned
-      query = query.toLowerCase();                                //query list initalization
-      
-      for(int i = 0; i < catalog.size(); i++){                    //loops through all catalog entries      
-         
-         String currentEntry = catalog.get(i).name.toLowerCase();
-         for(int j = 0; j < catalog.get(i).name.length(); j++){  //loops through characters within current entry
-            int holder = 0;
-            try{                                                     
-            //compares characters within querey char array with catalog product char array
-               if(currentEntry.charAt(j) == query.charAt(holder)){
-                  holder++;
-               }
-               else{
-                  holder = 0;
-               }
-            }
-            //if query array index out of bounds, all characters within the array have been compared and matched
-            //meaning it is a match and is added to the return list
-            catch(ArrayIndexOutOfBoundsException e){
-               returnList.add(catalog.get(i));
-            }
-         }
-      }
-      return returnList;
+   public void addToShoppingCart(ArrayList<Product> searchList, int index, ShoppingCart shoppingCart){
+      shoppingCart.addProduct(searchList.get(index));
+   }
+   public void addToWishList(ArrayList<Product> searchList, int index, WishList wishList){
+      wishList.addProduct(searchList.get(index));
    }
 }
